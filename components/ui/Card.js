9969e0daebeb9519ef40e57756d3f0574fdd8518
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-11 13:08:05 -0600

    added use window dimensions hook

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,9 +1,11 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions  } from "react-native";
 import Colors from '../../constants/colors';
 
 function Card({children}) {
   return <View style={styles.card}>{children}</View>;
 }
+
+const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   card: {
@@ -12,7 +14,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 24,
     borderRadius: 8,
     padding: 16,
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36 ,
     backgroundColor: Colors.primary800,
     elevation: 4,
     shadowColor: "black",
